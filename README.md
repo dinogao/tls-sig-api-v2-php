d7b765c0e08eb3a0e6aaa88313018d04c274e157
dinogao <dinogao@tencent.com>
2024-10-29 16:21:07 +0800

    update tencentyun to tencentcloud

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-## 说明
-此项目为 tls-sig-api-v2 版本 php 实现，之前非对称密钥无法使用此版本 api，如需使用请查看[这里](https://github.com/tencentyun/tls-sig-api-php)。
+## Note
+This project is the php implementation of tls-sig-api-v2. Previous asymmetric keys cannot use APIs of this version. To enable them to use APIs of this version,[see here](https://github.com/tencentcloud/tls-sig-api-php)。
 
-## 集成
-使用 composer 或者源码集成均可。
+## integration
+You can use composer or source code integration.
 
-### composer 集成
+### composer integration
 ``` json
 {
   "require": {
@@ -13,15 +13,15 @@
 }
 ```
 
-### 源码集成
-将 `TLSSigAPIv2.php` 下载至工程中即可。
+### source code integration
+Download `TLSSigAPIv2.php` to the project.
 
-## 使用
+## use
 ``` php
 <?php
 
 require 'vendor/autoload.php'
-// require_once "../src/TLSSigAPIv2.php"; // 源码集成使用相对路径 
+// require_once "../src/TLSSigAPIv2.php"; // Source code integration uses relative paths
 
 $api = new \Tencent\TLSSigAPIv2(1400000000, '5bd2850fff3ecb11d7c805251c51ee463a25727bddc2385f3fa8bfee1bb93b5e');
 $sig = $api->genUserSig('xiaojun');
--- src/TLSSigAPIv2.php
+++ src/TLSSigAPIv2.php
@@ -12,17 +12,7 @@ class TLSSigAPIv2 {
     private $sdkappid = 0;
 
     /**
-    *【功能说明】用于签发 TRTC 和 IM 服务中必须要使用的 UserSig 鉴权票据
-    *
-    *【参数说明】
-    * @param string userid - 用户id，限制长度为32字节，只允许包含大小写英文字母（a-zA-Z）、数字（0-9）及下划线和连词符。
-    * @param string expire - UserSig 票据的过期时间，单位是秒，比如 86400 代表生成的 UserSig 票据在一天后就无法再使用了。
-    * @return string 签名字符串
-    * @throws \Exception
-    */
-
-    /**
-     * Function: Used to issue UserSig that is required by the TRTC and IM services.
+     * Function: Used to issue UserSig that is required by the TRTC and CHAT services.
      *
      * Parameter description:
      * @param userid - User ID. The value can be up to 32 bytes in length and contain letters (a-z and A-Z), digits (0-9), underscores (_), and hyphens (-).
@@ -34,31 +24,6 @@ class TLSSigAPIv2 {
     public function genUserSig( $userid, $expire = 86400*180 ) {
         return $this->__genSig( $userid, $expire, '', false );
     }
-
-    /**
-    *【功能说明】
-    * 用于签发 TRTC 进房参数中可选的 PrivateMapKey 权限票据。
-    * PrivateMapKey 需要跟 UserSig 一起使用，但 PrivateMapKey 比 UserSig 有更强的权限控制能力：
-    *  - UserSig 只能控制某个 UserID 有无使用 TRTC 服务的权限，只要 UserSig 正确，其对应的 UserID 可以进出任意房间。
-    *  - PrivateMapKey 则是将 UserID 的权限控制的更加严格，包括能不能进入某个房间，能不能在该房间里上行音视频等等。
-    * 如果要开启 PrivateMapKey 严格权限位校验，需要在【实时音视频控制台】=>【应用管理】=>【应用信息】中打开“启动权限密钥”开关。
-    *
-    *【参数说明】
-    * @param userid - 用户id，限制长度为32字节，只允许包含大小写英文字母（a-zA-Z）、数字（0-9）及下划线和连词符。
-    * @param expire - PrivateMapKey 票据的过期时间，单位是秒，比如 86400 生成的 PrivateMapKey 票据在一天后就无法再使用了。
-    * @param roomid - 房间号，用于指定该 userid 可以进入的房间号
-    * @param privilegeMap - 权限位，使用了一个字节中的 8 个比特位，分别代表八个具体的功能权限开关：
-    *  - 第 1 位：0000 0001 = 1，创建房间的权限
-    *  - 第 2 位：0000 0010 = 2，加入房间的权限
-    *  - 第 3 位：0000 0100 = 4，发送语音的权限
-    *  - 第 4 位：0000 1000 = 8，接收语音的权限
-    *  - 第 5 位：0001 0000 = 16，发送视频的权限
-    *  - 第 6 位：0010 0000 = 32，接收视频的权限
-    *  - 第 7 位：0100 0000 = 64，发送辅路（也就是屏幕分享）视频的权限
-    *  - 第 8 位：1000 0000 = 200，接收辅路（也就是屏幕分享）视频的权限
-    *  - privilegeMap == 1111 1111 == 255 代表该 userid 在该 roomid 房间内的所有功能权限。
-    *  - privilegeMap == 0010 1010 == 42  代表该 userid 拥有加入房间和接收音视频数据的权限，但不具备其他权限。
-    */
 
     /**
      * Function:
@@ -89,31 +54,7 @@ class TLSSigAPIv2 {
         $userbuf = $this->__genUserBuf( $userid, $roomid, $expire, $privilegeMap, 0, '' );
         return $this->__genSig( $userid, $expire, $userbuf, true );
     }
-    /**
-    *【功能说明】
-    * 用于签发 TRTC 进房参数中可选的 PrivateMapKey 权限票据。
-    * PrivateMapKey 需要跟 UserSig 一起使用，但 PrivateMapKey 比 UserSig 有更强的权限控制能力：
-    *  - UserSig 只能控制某个 UserID 有无使用 TRTC 服务的权限，只要 UserSig 正确，其对应的 UserID 可以进出任意房间。
-    *  - PrivateMapKey 则是将 UserID 的权限控制的更加严格，包括能不能进入某个房间，能不能在该房间里上行音视频等等。
-    * 如果要开启 PrivateMapKey 严格权限位校验，需要在【实时音视频控制台】=>【应用管理】=>【应用信息】中打开“启动权限密钥”开关。
-    *
-    *【参数说明】
-    * @param userid - 用户id，限制长度为32字节，只允许包含大小写英文字母（a-zA-Z）、数字（0-9）及下划线和连词符。
-    * @param expire - PrivateMapKey 票据的过期时间，单位是秒，比如 86400 生成的 PrivateMapKey 票据在一天后就无法再使用了。
-    * @param roomstr - 房间号，用于指定该 userid 可以进入的房间号
-    * @param privilegeMap - 权限位，使用了一个字节中的 8 个比特位，分别代表八个具体的功能权限开关：
-    *  - 第 1 位：0000 0001 = 1，创建房间的权限
-    *  - 第 2 位：0000 0010 = 2，加入房间的权限
-    *  - 第 3 位：0000 0100 = 4，发送语音的权限
-    *  - 第 4 位：0000 1000 = 8，接收语音的权限
-    *  - 第 5 位：0001 0000 = 16，发送视频的权限
-    *  - 第 6 位：0010 0000 = 32，接收视频的权限
-    *  - 第 7 位：0100 0000 = 64，发送辅路（也就是屏幕分享）视频的权限
-    *  - 第 8 位：1000 0000 = 200，接收辅路（也就是屏幕分享）视频的权限
-    *  - privilegeMap == 1111 1111 == 255 代表该 userid 在该 roomid 房间内的所有功能权限。
-    *  - privilegeMap == 0010 1010 == 42  代表该 userid 拥有加入房间和接收音视频数据的权限，但不具备其他权限。
-    */
-
+    
     /**
      * Function:
      * Used to issue PrivateMapKey that is optional for room entry.
@@ -150,14 +91,6 @@ class TLSSigAPIv2 {
     }
 
     /**
-    * 用于 url 的 base64 encode
-    * '+' => '*', '/' => '-', '=' => '_'
-    * @param string $string 需要编码的数据
-    * @return string 编码后的base64串，失败返回false
-    * @throws \Exception
-    */
-
-    /**
     * base64 encode for url
     * '+' => '*', '/' => '-', '=' => '_'
     * @param string $string data to be encoded
@@ -172,14 +105,6 @@ class TLSSigAPIv2 {
         }
         return str_replace( array_keys( $replace ), array_values( $replace ), $base64 );
     }
-
-    /**
-    * 用于 url 的 base64 decode
-    * '+' => '*', '/' => '-', '=' => '_'
-    * @param string $base64 需要解码的base64串
-    * @return string 解码后的数据，失败返回false
-    * @throws \Exception
-    */
 
     /**
     * base64 decode for url
@@ -197,19 +122,7 @@ class TLSSigAPIv2 {
         }
         return $result;
     }
-    /**
-    * TRTC业务进房权限加密串使用用户定义的userbuf
-    * @brief 生成 userbuf
-    * @param account 用户名
-    * @param dwSdkappid sdkappid
-    * @param dwAuthID  数字房间号
-    * @param dwExpTime 过期时间：该权限加密串的过期时间. 过期时间 = now+dwExpTime
-    * @param dwPrivilegeMap 用户权限，255表示所有权限
-    * @param dwAccountType 用户类型, 默认为0
-    * @param roomStr 字符串房间号
-    * @return userbuf string  返回的userbuf
-    */
-
+    
     /**
     * User-defined userbuf is used for the encrypted string of TRTC service entry permission
     * @brief generate userbuf
@@ -255,16 +168,7 @@ class TLSSigAPIv2 {
         }
         return $userbuf;
     }
-    /**
-    * 使用 hmac sha256 生成 sig 字段内容，经过 base64 编码
-    * @param $identifier 用户名，utf-8 编码
-    * @param $curr_time 当前生成 sig 的 unix 时间戳
-    * @param $expire 有效期，单位秒
-    * @param $base64_userbuf base64 编码后的 userbuf
-    * @param $userbuf_enabled 是否开启 userbuf
-    * @return string base64 后的 sig
-    */
-
+    
     /**
     * Use hmac sha256 to generate sig field content, base64 encoded
     * @param $identifier Username, utf-8 encoded
@@ -285,17 +189,6 @@ class TLSSigAPIv2 {
         return base64_encode( hash_hmac( 'sha256', $content_to_be_signed, $this->key, true ) );
     }
 
-    /**
-    * 生成签名。
-    *
-    * @param $identifier 用户账号
-    * @param int $expire 过期时间，单位秒，默认 180 天
-    * @param $userbuf base64 编码后的 userbuf
-    * @param $userbuf_enabled 是否开启 userbuf
-    * @return string 签名字符串
-    * @throws \Exception
-    */
-    
     /**
     * Generate signature.
     *
@@ -338,19 +231,6 @@ class TLSSigAPIv2 {
     }
 
     /**
-    * 验证签名。
-    *
-    * @param string $sig 签名内容
-    * @param string $identifier 需要验证用户名，utf-8 编码
-    * @param int $init_time 返回的生成时间，unix 时间戳
-    * @param int $expire_time 返回的有效期，单位秒
-    * @param string $userbuf 返回的用户数据
-    * @param string $error_msg 失败时的错误信息
-    * @return boolean 验证是否成功
-    * @throws \Exception
-    */
-
-     /**
     * Verify signature.
     *
     * @param string $sig Signature content
@@ -418,18 +298,6 @@ class TLSSigAPIv2 {
     }
 
     /**
-    * 带 userbuf 验证签名。
-    *
-    * @param string $sig 签名内容
-    * @param string $identifier 需要验证用户名，utf-8 编码
-    * @param int $init_time 返回的生成时间，unix 时间戳
-    * @param int $expire_time 返回的有效期，单位秒
-    * @param string $error_msg 失败时的错误信息
-    * @return boolean 验证是否成功
-    * @throws \Exception
-    */
-
-    /**
     * Verify signature with userbuf.
     *
     * @param string $sig Signature content
@@ -444,18 +312,6 @@ class TLSSigAPIv2 {
         $userbuf = '';
         return $this->__verifySig( $sig, $identifier, $init_time, $expire_time, $userbuf, $error_msg );
     }
-
-    /**
-    * 验证签名
-    * @param string $sig 签名内容
-    * @param string $identifier 需要验证用户名，utf-8 编码
-    * @param int $init_time 返回的生成时间，unix 时间戳
-    * @param int $expire_time 返回的有效期，单位秒
-    * @param string $userbuf 返回的用户数据
-    * @param string $error_msg 失败时的错误信息
-    * @return boolean 验证是否成功
-    * @throws \Exception
-    */
 
     /**
     * Verify signature
